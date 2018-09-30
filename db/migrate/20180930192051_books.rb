33530afcbc4209dca538c2934bf1e0651188a6dc
ADONISXPS\adoni <adonisokpidi@hotmail.com>
2018-09-30 20:34:15 +0100

    create books table and added columns

--- db/migrate/20180930192051_books.rb
+++ db/migrate/20180930192051_books.rb
@@ -1,4 +1,5 @@
 class Books < ActiveRecord::Migration[5.1]
+
   def self.up
     create_table :books do |t|
       t.column :title, :string, :limit => 32, :null => false
@@ -7,9 +8,10 @@ class Books < ActiveRecord::Migration[5.1]
       t.column :description, :text
       t.column :created_at, :timestamp
     end
- end
+  end
 
   def self.down
     drop_table :books
   end
+
 end
